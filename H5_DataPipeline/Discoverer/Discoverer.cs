18935848d18733b030afca18a8dfcaddea34d1ff
Connor <knelly26@gmail.com>
2018-05-10 15:35:40 -0700

    Refactor Discoverer code that finds unaffiliated players.

--- H5_DataPipeline/Discoverer/Discoverer.cs
+++ H5_DataPipeline/Discoverer/Discoverer.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using H5_DataPipeline.Models.DataPipeline;
+using H5_DataPipeline.Assistants.Shared;
+using H5_DataPipeline.Assistants.CompanyRosters;
 
 
 namespace H5_DataPipeline.CompanyDiscovery
@@ -14,31 +16,31 @@ namespace H5_DataPipeline.CompanyDiscovery
         {
             List<t_h5matches_playersformatch> matchesWithPlayers = new List<t_h5matches_playersformatch>();
 
-            using(var db = new dev_spartanclashbackendEntities())
+            using (var db = new dev_spartanclashbackendEntities())
             {
                 matchesWithPlayers = db.t_h5matches_playersformatch.Take(1).ToList();
-                    //.Where(match => match.discoveryRan == null).ToList();
+                //.Where(match => match.discoveryRan == null).ToList();
 
                 Console.WriteLine("Found {0} matches to search through.", matchesWithPlayers.Count);
             }
 
-            foreach(t_h5matches_playersformatch match in matchesWithPlayers)
-            {
-                Console.WriteLine("MatchID: {0}", match.matchID);
-                List<string> unaffiliatedPlayers = match.GetAllUnaffiliatedGamertagsInMatch();
+            List<string> unaffiliatedPlayers = GetAllUnaffiliatedPlayersinMatchSet(matchesWithPlayers);
 
-                PrintListofString(unaffiliatedPlayers);
-            }
+            ScanPlayersForNewCompanies(unaffiliatedPlayers);
 
-
+            unaffiliatedPlayers.ForEach(item => Console.WriteLine(item));
         }
 
-        private void PrintListofString(List<string> list)
+        private List<string> GetAllUnaffiliatedPlayersinMatchSet(List<t_h5matches_playersformatch> matchSet)
         {
-            foreach(string item in list)
+            List<string> result = new List<string>();
+
+            foreach(t_h5matches_playersformatch match in matchSet)
             {
-                Console.WriteLine(item);
+                result.AddRange(match.GetAllUnaffiliatedGamertagsInMatch());
             }
+            
+            return result;
         }
     }
 }
